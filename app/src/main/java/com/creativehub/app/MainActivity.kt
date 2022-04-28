4865b1356378a3f3f5b69dc6965c47007e0c18a1
Alessandro Contenti <a.contenti@gmail.com>
2022-04-28 16:11:35 +0200

    initial commit
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

---
+++ app/src/main/java/com/creativehub/app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.creativehub.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.creativehub.app.ui.theme.CreativeHubTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			CreativeHubTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colors.background
+				) {
+					Greeting("Android")
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String) {
+	Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+	CreativeHubTheme {
+		Greeting("Android")
+	}
+}
